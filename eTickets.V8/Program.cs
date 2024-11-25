20fad57e25e563a571b37638df5b15575fcdfc10
Dunja <dunja.pavlovic.31@gmail.com>
2024-11-25 13:09:09 +0100

    Add project files.

---
+++ eTickets.V8/Program.cs
@@ -0,0 +1,42 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//ovde dodajem service nakon var builder linije ali pre var app linije
+
+
+
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddCors();  // --------------DODATO------------------
+
+var app = builder.Build();
+
+//nakon ove linije ali pre app.Run dodajem http.requests
+
+
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseCors();   // --------------DODATO------------------
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
